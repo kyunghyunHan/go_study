e4a42e65652e1c2152b9685cb46326582e3961a9
kyunhyun94 <ekfqlwcjswl@naver.com>
2021-10-27 16:31:37 +0900

    account

--- src/github.com/kyunghyun/learngo/accounts/accounts.go
+++ src/github.com/kyunghyun/learngo/accounts/accounts.go
@@ -1,18 +1,45 @@
 package accounts
 
+import (
+	"errors"
+	"fmt"
+)
+
 type Account struct {
 	owner   string
 	balance int
 }
 
+var errNoMony = errors.New("Can;t withdrew")
+
 func NewAccount(owner string) *Account {
 	account := Account{owner: owner, balance: 0}
 	return &account
 }
-func (a Account) Deposit(amount int) {
+func (a *Account) Deposit(amount int) {
 	a.balance += amount
 }
 
-func (a *Account) Balance() int {
+func (a Account) Balance() int {
 	return a.balance
+}
+
+func (a *Account) Withdraw(amount int) error {
+	if a.balance < amount {
+		return errNoMony
+	}
+	a.balance -= amount
+	return nil
+}
+
+func (a *Account) ChangeOwner(newOwner string) {
+	a.owner = newOwner
+}
+
+func (a Account) Owner() string {
+	return a.owner
+}
+
+func (a Account) String() string {
+	return fmt.Sprint(a.Owner(), "'s account.\nHas:", a.Balance())
 }
--- src/github.com/kyunghyun/learngo/main.go
+++ src/github.com/kyunghyun/learngo/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/kyunghyun/learngo/accounts"
 )
@@ -10,4 +11,10 @@ func main() {
 	account := accounts.NewAccount("nico")
 	account.Deposit(10)
 	fmt.Println(account.Balance())
+	account.Withdraw(20)
+	err := account.Withdraw(20)
+	if err != nil {
+		log.Println(err)
+	}
+	fmt.Println(account.Balance(), account.Owner())
 }
