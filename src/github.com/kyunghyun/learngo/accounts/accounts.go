4cceccf3015d9d050d5a7a3f0961acda6f7dffcb
kyunhyun94 <ekfqlwcjswl@naver.com>
2021-10-27 16:07:51 +0900

    add

---
+++ src/github.com/kyunghyun/learngo/accounts/accounts.go
@@ -0,0 +1,18 @@
+package accounts
+
+type Account struct {
+	owner   string
+	balance int
+}
+
+func NewAccount(owner string) *Account {
+	account := Account{owner: owner, balance: 0}
+	return &account
+}
+func (a Account) Deposit(amount int) {
+	a.balance += amount
+}
+
+func (a *Account) Balance() int {
+	return a.balance
+}
