590a70b272eb7fdabe4eca41306b89d1148f84a2
kyunhyun94 <ekfqlwcjswl@naver.com>
2021-10-29 12:41:51 +0900

    add

--- src/github.com/kyunghyun/learngo/main.go
+++ src/github.com/kyunghyun/learngo/main.go
@@ -1,95 +1,27 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
-	"strconv"
+	"os"
 	"strings"
 
-	"github.com/PuerkitoBio/goquery"
+	"github.com/kyunghyun/learngo/scrapper"
+	"github.com/labstack/echo"
 )
 
-type extractedJob struct {
-	id       string
-	title    string
-	location string
-	salary   string
-	summary  string
+const fileName string = "jobs.csv"
+
+func handleHome(c echo.Context) error {
+	return c.File("Home.html")
 }
-
-var baseURL string = "https://kr.indeed.com/jobs?q=python&limit=50"
-
+func handleScrape(c echo.Context) error {
+	defer os.Remove("fileName")
+	term := strings.ToLower(scrapper.CleanString(c.FormValue("term")))
+	scrapper.Scrape(term)
+	return c.Attachment("fileName", "fileName")
+}
 func main() {
-	var jobs []extractedJob
-	totalPages := getPages()
-	for i := 0; i < totalPages; i++ {
-		extractJob := getPage(i)
-		jobs = append(jobs, extractJob...)
-	}
-	fmt.Println(jobs)
-}
-func getPage(page int) []extractedJob {
-	var jobs []extractedJob
-	pageURL := baseURL + "&start=" + strconv.Itoa(page*50)
-	fmt.Println("Requesting", pageURL)
-	res, err := http.Get(pageURL)
-	checkErr(err)
-	checkCode(res)
-	defer res.Body.Close()
-	doc, err := goquery.NewDocumentFromReader(res.Body)
-
-	checkErr(err)
-	searchCards := doc.Find(".mosaic-zone")
-	searchCards.Each(func(i int, card *goquery.Selection) {
-		job := extractJob(card)
-		jobs = append(jobs, job)
-
-	})
-	return jobs
-}
-
-func extractJob(card *goquery.Selection) extractedJob {
-	id, _ := card.Attr("data-jk")
-	title := cleanString(card.Find("mosaic-provider-jobcards>a").Text())
-	location := cleanString(card.Find(".sjcl").Text())
-	salary := cleanString(card.Find(".salaryText").Text())
-	summary := cleanString(card.Find(".summary").Text())
-	return extractedJob{
-		id:       id,
-		title:    title,
-		location: location,
-		salary:   salary,
-		summary:  summary}
-}
-
-func cleanString(str string) string {
-	return strings.Join(strings.Fields(strings.TrimSpace(str)), "")
-}
-func getPages() int {
-	pages := 0
-	res, err := http.Get(baseURL)
-
-	checkErr(err)
-	checkCode(res)
-	defer res.Body.Close()
-	doc, err := goquery.NewDocumentFromReader(res.Body)
-
-	checkErr(err)
-	doc.Find(".pagination").Each(func(i int, s *goquery.Selection) {
-		pages = s.Find("a").Length()
-	})
-	return pages
-}
-
-func checkErr(err error) {
-	if err != nil {
-		log.Fatalln(err)
-	}
-}
-
-func checkCode(res *http.Response) {
-	if res.StatusCode != 200 {
-		log.Fatalln("Request failed with Status", res.StatusCode)
-	}
+	e := echo.New()
+	e.GET("/", handleHome)
+	e.POST("/scrape", handleScrape)
+	e.Logger.Fatal(e.Start(":1323"))
 }
